80c96f10f759321e38a9c2925894a6c530599ac8
Poll0none <126181078+Poll0none@users.noreply.github.com>
2023-09-29 00:26:34 +0200

    Update Trade-PrintChatMessage.lua

--- AdoptMe/Trade-PrintChatMessage.lua
+++ AdoptMe/Trade-PrintChatMessage.lua
@@ -4,15 +4,16 @@ This code prints last ChatMessage from trade chat: LOCATED AT game:GetService("P
 It will need to be trimmed so I can extract only the pet´s name and assign it to a variable, also I need to 
 ]]--
 
+--Execute getgenv().toggle = false to disable While Loop
+
 local player = game:GetService("Players").LocalPlayer
 local chatFrame = player.PlayerGui.TradeApp.Frame.NegotiationFrame.ChatFrame
 local chatLog = chatFrame.ChatLog.ScrollComplex.ScrollingFrame.Content
 local lastMessage = nil
 
-
 getgenv().toggle = true
 
-while wait(1) and toggle == true do
+while wait() and toggle == true do
     local newMessage = nil
     for _, chatMessage in ipairs(chatLog:GetChildren()) do
         if chatMessage:IsA("TextLabel") and chatMessage.Name == "ChatMessage" then
@@ -25,7 +26,9 @@ while wait(1) and toggle == true do
         lastMessage = newMessage
     end
 
-    wait(1) -- Adjust the wait time as needed to control how frequently you check for new messages
+    wait() -- Adjust the wait time as needed to control how frequently you check for new messages
 end
+
+
 
 
