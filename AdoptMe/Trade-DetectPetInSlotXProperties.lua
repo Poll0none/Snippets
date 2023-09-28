5b6a56af87511a06cfb23edb19407e95eb56f9d6
Poll0none <126181078+Poll0none@users.noreply.github.com>
2023-09-29 00:21:29 +0200

    Update Trade-DetectPetInSlotXProperties.lua

--- AdoptMe/Trade-DetectPetInSlotXProperties.lua
+++ AdoptMe/Trade-DetectPetInSlotXProperties.lua
@@ -3,6 +3,7 @@ local player = game:GetService("Players").LocalPlayer
 --Change .flyable for rideable or mega_neon or neon THERE ARE OTHER VALUES mega_neon_old and wearing
 local flyable = player.PlayerGui.TradeApp.Frame.NegotiationFrame.Body.PartnerOffer.Slots.Slot2.TagDisplayTemplate.flyable
 
+--Check if IsLoaded is true meaning the property is present in this pet
 if flyable.IsLoaded == true then
     print("Flyable")
 end
