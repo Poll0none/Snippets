2224f13065903b5d1609d32f3306764732d05a17
Poll0none <126181078+Poll0none@users.noreply.github.com>
2023-09-29 19:06:52 +0200

    Create CHAT-PrintServerChatLog.lua

---
+++ CHAT-PrintServerChatLog.lua
@@ -0,0 +1,14 @@
+--[[
+Game Chat logger, could be interesting to find what people are trading
+]]--
+
+game.Players.PlayerChatted:Connect(function(ChatType, PlayerWhoSentMsg, Msg, RecipientIfMsgIsPM) 
+    print(("%s just said '%s'"):format(PlayerWhoSentMsg.name, Msg))
+    
+    if Msg:sub(1,4) == ";cmd" then
+        -- do whatever
+    elseif Msg:sub(1,5) == ";help" then
+        -- do whatever
+    -- etc.
+    end
+end)
