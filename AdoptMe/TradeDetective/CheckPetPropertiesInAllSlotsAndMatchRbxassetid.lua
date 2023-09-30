cd15ccc44c7d93b3c003a285348f23a30496d9d9
Poll0none <126181078+Poll0none@users.noreply.github.com>
2023-09-30 12:52:58 +0200

    Update CheckPetPropertiesInAllSlotsAndMatchRbxassetid.lua

--- AdoptMe/TradeDetective/CheckPetPropertiesInAllSlotsAndMatchRbxassetid.lua
+++ AdoptMe/TradeDetective/CheckPetPropertiesInAllSlotsAndMatchRbxassetid.lua
@@ -1,20 +1,22 @@
 --[[
 CHECKS ALL SLOTS, and if there is a coincidence with the table petData then it prints I found a coincidence. Loops through all slots up to 9
 ]]--
-
 local player = game:GetService("Players").LocalPlayer
 
 local petData = {
-    petname1 = 13620861015,
-    petname2 = 13894724323,
-    petname3 = 10618035564,
+    alicorn = 9781852671,
+    brown_chow = 9938967576,
+    golden_king_penguin = 9490242683,
+    golden_albatross = 9475753542,
+    golden_albatross = 9475753542,
     -- Add more pet names and rbxassetid pairs as needed
 }
 
 -- Function to check for coincidences
 local function checkForCoincidences(rbxassetid)
     for petName, petRbxassetid in pairs(petData) do
-        if petRbxassetid == rbxassetid then
+        -- Convert both rbxassetids to strings and compare
+        if tostring(petRbxassetid) == tostring(rbxassetid) then
             return petName -- Return the pet name if there is a coincidence
         end
     end
@@ -55,7 +57,7 @@ local numIterations = 9 -- Change this to the number of iterations you want
 for slotCounter = 1, numIterations do
     local success, result = pcall(checkSlotProperties, slotCounter)
     
-    if success then
+    if success and result then
         -- Store the result in TradeSlots[slotCounter]
         TradeSlots[slotCounter] = result
 
@@ -66,20 +68,24 @@ for slotCounter = 1, numIterations do
         print("Ride:", TradeSlots[slotCounter].rideable)
         print("Neon:", TradeSlots[slotCounter].neon)
         
-        --Mega NEON always prints false, IDKW, maybe I have to check for old_mega_neon
+        -- Mega NEON always prints false, IDKW, maybe I have to check for old_mega_neon
         print("Mega:", TradeSlots[slotCounter].mega_neon)
     end
 end
 
-
---Checks all slots to see if any rbxassetid coincides with the table defined at the start of this code: petData
-for petName, petRbxassetid in pairs(petData) do
-    local coincidence = checkForCoincidences(petRbxassetid)
-    if coincidence then
-        print("Coincidence found!")
-        print("Pet Name:", coincidence)
-        print("rbxassetid:", petRbxassetid)
-    else
-        print("No coincidence found for rbxassetid:", petRbxassetid)
+-- Check for coincidences after TradeSlots are populated
+for slotCounter = 1, numIterations do
+    local rbxassetid = TradeSlots[slotCounter] and TradeSlots[slotCounter].rbxassetid
+    if rbxassetid then
+        local coincidence = checkForCoincidences(rbxassetid)
+        
+        if coincidence then
+            print("Coincidence found for Slot " .. slotCounter .. ":")
+            print("Pet Name:", coincidence)
+            print("rbxassetid:", rbxassetid)
+        else
+            print("No coincidence found for Slot " .. slotCounter .. ":")
+            print("rbxassetid:", rbxassetid)
+        end
     end
 end
