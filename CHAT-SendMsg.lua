4694e3fc9e7c9147a583f538a2a99f96c8f3d8b7
Poll0none <126181078+Poll0none@users.noreply.github.com>
2023-10-05 10:42:32 +0200

    Update CHAT-SendMsg.lua

--- CHAT-SendMsg.lua
+++ CHAT-SendMsg.lua
@@ -1,3 +1,3 @@
-local Framework = loadstring(game:HttpGet("https://raw.githubusercontent.com/Poll0none/Snippets/main/CHAT-Framework.lua?token=GHSAT0AAAAAACH5SFKQB4IBS4OE3ZZNSH7WZI6N4ZQ", true))()
+local Framework = loadstring(game:HttpGet("https://raw.githubusercontent.com/Poll0none/Snippets/main/CHAT-Framework.lua", true))()
 local services = Framework.Services
 services.TextChatService.TextChannels["RBXGeneral"]:SendAsync("hi")
